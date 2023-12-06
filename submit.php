e38e90f4ba4468036dd2890b2cfee632310fc436
nxtjenllc <150939025+nxtjenllc@users.noreply.github.com>
2023-12-06 15:31:04 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -2,51 +2,77 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Include PHPMailer Autoload
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+// Load PHPMailer classes
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 
+// Function to extract lawyer's name and email from the selected option
+function extractLawyerInfo($selectedOption) {
+    // Adjust this regex pattern based on the actual format of your dropdown options
+    preg_match('/(.+) \((.+)\)/', $selectedOption, $matches);
+
+    if ($matches && count($matches) === 3) {
+        return array('name' => $matches[1], 'email' => $matches[2]);
+    }
+
+    return null;
+}
+
+// Assuming the lawyer reaching option is submitted through a POST request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $selectedLawyer = $_POST['lawyer_reaching'];
-    $yourEmail = 'nxtjenanswering@gmail.com'; // Replace with your Gmail email address
-    $yourPassword = 'April16$1987!';     // Replace with your Gmail password
+    // Retrieve the selected lawyer reaching option from the form submission
+    $selectedLawyerOption = $_POST['lawyer_reaching'];
 
-    $to = $selectedLawyer;
-    $cc = $yourEmail;
-    $subject = "New Legal Consultation Form Submission";
-    
-    $message = "Name: {$_POST['name']}\n";
-    // Include other form data in the email body
+    // Extract lawyer's name and email from the selected option
+    $lawyerInfo = extractLawyerInfo($selectedLawyerOption);
 
-    $mail = new PHPMailer(true);
+    if ($lawyerInfo) {
+        try {
+            // Create a new PHPMailer instance
+            $mail = new PHPMailer(true);
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                      // Enable verbose debug output (set to 2 for detailed debug)
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = nxtjenanswering@gmail.com;
-        $mail->Password = April16$1987!;
-        $mail->SMTPSecure = 'tls'; // Use 'tls' or 'ssl'
-        $mail->Port = 465;         // Set the appropriate port: 587 for TLS, 465 for SSL
+            // Set mailer to use SMTP
+            $mail->isSMTP();
+            
+            // Specify the SMTP server
+            $mail->Host = 'smtp.gmail.com';
 
-        //Recipients
-        $mail->setFrom($yourEmail);
-        $mail->addAddress($to);
-        $mail->addCC($cc);
+            // Enable SMTP debugging
+            // $mail->SMTPDebug = 2; // Uncomment this line for debugging purposes
+            
+            // Set SMTP authentication
+            $mail->SMTPAuth = true;
+            
+            // Specify the SMTP username
+            $mail->Username = 'nxtjenanswering@gmail.com';
+            
+            // Specify the SMTP password
+            $mail->Password = 'April16$1987!';
+            
+            // Enable TLS encryption; `PHPMailer::ENCRYPTION_STARTTLS` also accepted
+            $mail->SMTPSecure = 'tls';
+            
+            // Set the SMTP port
+            $mail->Port = 587;
 
-        // Content
-        $mail->isHTML(false); // Set to true if you're sending HTML content
-        $mail->Subject = $subject;
-        $mail->Body = $message;
+            // Set sender and recipient information
+            $mail->setFrom('nxtjenanswering@gmail.com', 'Your Name');
+            $mail->addAddress($lawyerInfo['email'], $lawyerInfo['name']);
 
-        $mail->send();
-        header("Location: thank-you.html");
-        exit();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            // Set email subject and body
+            $mail->Subject = 'Test Subject';
+            $mail->Body    = 'This is a test email from PHPMailer.';
+
+            // Send the email
+            $mail->send();
+
+            echo 'Email has been sent successfully';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
+        echo 'Error: Unable to extract lawyer information from the selected option';
     }
 }
 ?>
